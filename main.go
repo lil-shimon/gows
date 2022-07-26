1ac84bef8533c8568ff32318748ec2ce072e9d96
Kenta Shimosawa <shimon101018@gmail.com>
2022-07-26 16:27:27 +0900

    feat: 🎸 simple server with echo

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func handleServer(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello")
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.GET("/", handleServer)
+	e.Logger.Fatal(e.Start(":1323"))
+}
