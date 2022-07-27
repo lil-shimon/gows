825558798dd230c7725a61d3279cbd5d3b0c4f85
Kenta Shimosawa <shimon101018@gmail.com>
2022-07-27 09:54:29 +0900

    feat: 🎸 send json

--- main.go
+++ main.go
@@ -5,9 +5,16 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"golang.org/x/net/websocket"
+	"math/rand"
 	"net/http"
 	"time"
 )
+
+type NVDate struct {
+	Noise       float64
+	Vibration   float64
+	MeasureDate time.Time
+}
 
 func handleServer(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello")
@@ -42,7 +49,11 @@ func sendDataEachSecond(c echo.Context) error {
 	websocket.Handler(func(ws *websocket.Conn) {
 		defer ws.Close()
 		for range time.Tick(1000 * time.Millisecond) {
-			err := websocket.Message.Send(ws, "noise")
+			var nv NVDate
+			nv.Noise = rand.Float64()
+			nv.Vibration = rand.Float64()
+			nv.MeasureDate = time.Now()
+			err := websocket.JSON.Send(ws, nv)
 			if err != nil {
 				c.Logger().Error(err)
 			}
