111650128582b9e175e92acbb8cd88b697e8dc29
Kenta Shimosawa <shimon101018@gmail.com>
2022-07-27 09:41:53 +0900

    feat: 🎸 ws send data each second

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"golang.org/x/net/websocket"
+	"net/http"
+	"time"
 )
 
 func handleServer(c echo.Context) error {
@@ -38,11 +38,25 @@ func handleWS(c echo.Context) error {
 	return nil
 }
 
+func sendDataEachSecond(c echo.Context) error {
+	websocket.Handler(func(ws *websocket.Conn) {
+		defer ws.Close()
+		for range time.Tick(1000 * time.Millisecond) {
+			err := websocket.Message.Send(ws, "noise")
+			if err != nil {
+				c.Logger().Error(err)
+			}
+		}
+	}).ServeHTTP(c.Response(), c.Request())
+	return nil
+}
+
 func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.GET("/", handleServer)
 	e.Static("/ws-client", "public")
 	e.GET("/ws-client/ws", handleWS)
+	e.GET("/ws/noise", sendDataEachSecond)
 	e.Logger.Fatal(e.Start(":1323"))
 }
